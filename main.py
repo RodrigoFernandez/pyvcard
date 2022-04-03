20851981c669c5125110cbe5046c83d66b8e6287
Rodrigo <rdr.rkh+github2@gmail.com>
2022-04-03 19:23:54 -0300

    Se agregó la lógica para que se pueda poner un logo en el centro del svg generado como png

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from pyvcard import pyvcard
 
+
+def crear_qr_qrcode():
+    vcard = pyvcard.generar_vcard({'apellido': 'Reiris', 'nombre': 'Gaston', 'prefijo': 'Dr.'}, {'nombre': 'Será Parrilla', 'puesto': 'CEO y Gerente de Baños'},
+                                  {'telefono': '4444-5555', 'mail': 'ggreiris@seraparrilla.com.ar', 'site': "https://seraparrilla.netlify.app/"})
+    print(vcard)
+    pyvcard.generar_qr(vcard, "./qr_ggreiris.png")
+    pyvcard.generar_qr(vcard, "./qr_ggreiris_logo.png", "./logo.png")
+
+    pyvcard.generar_qr_svgimage(vcard, "./qr_svgimage.svg")
+    pyvcard.generar_qr_svgfragmentImage(vcard, "./qr_svgfragmentImage.svg")
+    pyvcard.generar_qr_svgpathimage(vcard, "./qr_svgpathimage.svg")
+
+
 if __name__ == "__main__":
-	#pyvcard.generar_qr("https://seraparrilla.netlify.app/", "./qr_seraparrilla.png")
-	vcard = pyvcard.generar_vcard({'apellido': 'Reiris', 'nombre': 'Gaston', 'prefijo': 'Dr.'}, {'nombre': 'Será Parrilla', 'puesto': 'CEO y Gerente de Baños'}, {'telefono': '4444-5555', 'mail': 'ggreiris@seraparrilla.com.ar', 'site': "https://seraparrilla.netlify.app/"})
-	print(vcard)
-	pyvcard.generar_qr(vcard, "./qr_ggreiris.png")
-	
-	pyvcard.generar_qr_svgimage(vcard, "./qr_svgimage.svg")
-	pyvcard.generar_qr_svgfragmentImage(vcard, "./qr_svgfragmentImage.svg")
-	pyvcard.generar_qr_svgpathimage(vcard, "./qr_svgpathimage.svg")
-	
+    crear_qr_qrcode()
--- pyvcard/pyvcard.py
+++ pyvcard/pyvcard.py
@@ -1,6 +1,7 @@
 import qrcode
 import qrcode.image.svg
 import vobject
+from PIL import Image
 
 
 # https://towardsdatascience.com/generate-qrcode-with-python-in-5-lines-42eda283f325
@@ -10,52 +11,65 @@ import vobject
 
 
 def generar_qr_svg(fuente, destino, factory):
-	img = qrcode.make(fuente, image_factory=factory)
-	img.save(destino)
+    img = qrcode.make(fuente, image_factory=factory)
+    img.save(destino)
+
 
 def generar_qr_svgimage(fuente, destino):
-	generar_qr_svg(fuente, destino, qrcode.image.svg.SvgImage)
-	
-def generar_qr_svgfragmentImage(fuente, destino):
-	generar_qr_svg(fuente, destino, qrcode.image.svg.SvgFragmentImage)
-	
-def generar_qr_svgpathimage(fuente, destino):
-	generar_qr_svg(fuente, destino, qrcode.image.svg.SvgPathImage)
+    generar_qr_svg(fuente, destino, qrcode.image.svg.SvgImage)
 
-def generar_qr(fuente, destino):
-	qr = qrcode.QRCode(version=1, box_size=10, border=5)
-	qr.add_data(fuente)
-	qr.make(fit=True)
-	
-	img = qr.make_image(fill='black', back_color='white')
-	img.save(destino)
+
+def generar_qr_svgfragmentImage(fuente, destino):
+    generar_qr_svg(fuente, destino, qrcode.image.svg.SvgFragmentImage)
+
+
+def generar_qr_svgpathimage(fuente, destino):
+    generar_qr_svg(fuente, destino, qrcode.image.svg.SvgPathImage)
+
+
+def generar_qr(fuente, destino, logo_path=None):
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(fuente)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill='black', back_color='white')
+
+    if logo_path:
+        logo = Image.open(logo_path)
+        pos = ((img.size[0] - logo.size[0]) // 2,
+               (img.size[1] - logo.size[1]) // 2)
+        img.paste(logo, pos)
+
+    img.save(destino)
+
 
 def generar_vcard(titular, empresa, contacto):
-	rta = vobject.vCard()
-	
-	rta.add('n')
-	rta.n.value = vobject.vcard.Name(family=titular['apellido'], given=titular['nombre'], prefix=titular['prefijo'])
-	
-	rta.add('fn')
-	rta.fn.value = f"{titular['nombre']} {titular['apellido']}"
-	
-	rta.add('org')
-	rta.org.value = [empresa['nombre']]
-	
-	rta.add('title')
-	rta.title.value = empresa['puesto']
-	
-	rta.add('tel')
-	rta.tel.value = contacto['telefono']
-	rta.tel.type_param = 'TRABAJO'
-	rta.tel.pref_param = '1'
-	
-	rta.add('email')
-	rta.email.value = contacto['mail']
-	rta.email.type_param = 'TRABAJO'
-	rta.email.pref_param = '1'
-	
-	rta.add('url')
-	rta.url.value = contacto['site']
-	
-	return rta.serialize()
+    rta = vobject.vCard()
+
+    rta.add('n')
+    rta.n.value = vobject.vcard.Name(
+        family=titular['apellido'], given=titular['nombre'], prefix=titular['prefijo'])
+
+    rta.add('fn')
+    rta.fn.value = f"{titular['nombre']} {titular['apellido']}"
+
+    rta.add('org')
+    rta.org.value = [empresa['nombre']]
+
+    rta.add('title')
+    rta.title.value = empresa['puesto']
+
+    rta.add('tel')
+    rta.tel.value = contacto['telefono']
+    rta.tel.type_param = 'TRABAJO'
+    rta.tel.pref_param = '1'
+
+    rta.add('email')
+    rta.email.value = contacto['mail']
+    rta.email.type_param = 'TRABAJO'
+    rta.email.pref_param = '1'
+
+    rta.add('url')
+    rta.url.value = contacto['site']
+
+    return rta.serialize()
